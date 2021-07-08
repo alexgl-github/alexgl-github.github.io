88faec7c97238854246a8efa1727760aa04f9c87
alex G <gladkova@lab126.com>
2021-07-08 08:46:03 -0700

    softmax

---
+++ src/plot.py
@@ -0,0 +1,34 @@
+import matplotlib
+matplotlib.rcParams['text.usetex'] = True
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator
+import numpy as np
+
+fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+
+# Make data.
+X = np.arange(-10, 10, 0.1)
+Y = np.arange(-10, 10, 0.1)
+X, Y = np.meshgrid(X, Y)
+Z = np.exp(X) / (np.exp(X) + np.exp(Y))
+
+# Plot the surface.
+surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+
+# Customize the z axis.
+ax.set_zlim(-0.1, 1.01)
+ax.zaxis.set_major_locator(LinearLocator(10))
+# A StrMethodFormatter is used automatically
+ax.zaxis.set_major_formatter('{x:.02f}')
+
+ax.set_xlabel(r'$X_{1}$', fontsize=20)
+ax.set_ylabel(r'$X_{2}$', fontsize=20)
+ax.zaxis.set_rotate_label(False)
+ax.set_zlabel(r'$Y_{1}$', fontsize=20, rotation=0)
+
+# Add a color bar which maps values to colors.
+#fig.colorbar(surf, shrink=0.5, aspect=10)
+
+plt.show()
