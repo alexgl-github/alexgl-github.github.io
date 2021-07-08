e8d8b4231be37be05fbb550a5c62d2514b0c634b
alex G <gladkova@lab126.com>
2021-07-08 08:59:44 -0700

    softmax

--- src/plot.py
+++ src/plot.py
@@ -18,15 +18,15 @@ surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
                        linewidth=0, antialiased=False)
 
 # Customize the z axis.
-ax.set_zlim(-0.1, 1.01)
+ax.set_zlim(-0.1, 1.1)
 ax.zaxis.set_major_locator(LinearLocator(10))
 # A StrMethodFormatter is used automatically
 ax.zaxis.set_major_formatter('{x:.02f}')
 
-ax.set_xlabel(r'$X_{1}$', fontsize=20)
-ax.set_ylabel(r'$X_{2}$', fontsize=20)
+ax.set_xlabel(r'$x_{1}$', fontsize=20)
+ax.set_ylabel(r'$x_{2}$', fontsize=20)
 ax.zaxis.set_rotate_label(False)
-ax.set_zlabel(r'$Y_{1}$', fontsize=20, rotation=0)
+ax.set_zlabel(r'$\sigma(x_{1})$', fontsize=20, rotation=0, labelpad=20)
 
 # Add a color bar which maps values to colors.
 #fig.colorbar(surf, shrink=0.5, aspect=10)
